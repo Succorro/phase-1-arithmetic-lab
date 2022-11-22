340b94f3ae52a53c6fc1a44ea0e7a5cb498b11c2
Steven Gutierrez <stevogbmv@gmail.com>
2022-11-22 15:04:46 -0800

    commit tree 2615085ba7673e39b6c721e910a724d062f5c701

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+var num1 = 2;
+var num2 = 31;
+var num3 = 16;
+var num4 = 6;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*10) + 1;
+let mod = num3 % num4;
+let max = Math.max(2,3,4,20);
